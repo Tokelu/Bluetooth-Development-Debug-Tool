817c88d2814bfac89995866c8f0cf3b5501e3361
Tonni <tonni@witzell.dk>
2019-11-14 13:56:28 +0100

    Initial Push

---
+++ app/src/main/java/da/au_grp21/bluetoothdevelopmentdebugtool/MainActivity.java
@@ -0,0 +1,16 @@
+package da.au_grp21.bluetoothdevelopmentdebugtool;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
