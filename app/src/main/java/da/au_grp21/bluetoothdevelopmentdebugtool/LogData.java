4506992503d92c36f94a903e7dff9b648482ebb5
Steen <steen.nisker@gmail.com>
2019-11-28 12:21:53 +0100

    First setup of database. ATTENTION: Includes changes to .gradle files, be careful with dependencies. - Includes following classes    * DatabaseService:       An intent service, which includes async calls to the database (firebase firestore).       Still needs some sort of lookup in the database.    * LogData:       Data class for the logs saved and loaded from the database.       Made serializable with the purpose of passing it through intents.

---
+++ app/src/main/java/da/au_grp21/bluetoothdevelopmentdebugtool/LogData.java
@@ -0,0 +1,41 @@
+package da.au_grp21.bluetoothdevelopmentdebugtool;
+
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+//Data class for representing the log files in the database
+public class LogData implements Serializable {
+    private String filename, terminalLog, timestamp;
+
+    public LogData () {}
+
+    public String getFilename() {
+        return filename;
+    }
+
+    public void setFilename(String filename) {
+        this.filename = filename;
+    }
+
+    public String getTerminalLog() {
+        return terminalLog;
+    }
+
+    public void setTerminalLog(String terminalLog) {
+        this.terminalLog = terminalLog;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    //Custom method for making the timestamp the current time
+    public void setTimestampNow(){
+        timestamp = new Timestamp(System.currentTimeMillis()).toString();
+    }
+}
