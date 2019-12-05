9a39270f8bc1267904f60abcae23c108830452bb
Tonni <tonni@witzell.dk>
2019-12-05 12:32:15 +0100

    Auto stash before rebase of "origin/master"

--- app/src/main/java/da/au_grp21/bluetoothdevelopmentdebugtool/Fragment/FragmentLoad.java
+++ app/src/main/java/da/au_grp21/bluetoothdevelopmentdebugtool/Fragment/FragmentLoad.java
@@ -1,7 +1,5 @@
 package da.au_grp21.bluetoothdevelopmentdebugtool.Fragment;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -67,7 +65,7 @@ public class FragmentLoad extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_fragment_load, container, false);
+        return inflater.inflate(R.layout.fragment_load_data, container, false);
     }
 
 
