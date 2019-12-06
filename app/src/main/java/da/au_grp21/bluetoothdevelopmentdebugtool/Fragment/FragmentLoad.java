e0f5f1f5680656dbdc70249761ffd30ff36f3c96
Mette <mettechrisjacobsen@hotmail.com>
2019-12-06 09:28:39 +0100

    *Code clean up
    *Dette er et dejligt FRÆKment*

--- app/src/main/java/da/au_grp21/bluetoothdevelopmentdebugtool/Fragment/FragmentLoad.java
+++ app/src/main/java/da/au_grp21/bluetoothdevelopmentdebugtool/Fragment/FragmentLoad.java
@@ -21,8 +21,6 @@ import da.au_grp21.bluetoothdevelopmentdebugtool.ViewModel.MyViewModel;
 /*
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link FragmentLoad.OnFragmentInteractionListener} interface
- * to handle interaction events.
  * Use the {@link FragmentLoad#newInstance} factory method to
  * create an instance of this fragment.
  */
@@ -116,21 +114,6 @@ public class FragmentLoad extends Fragment {
     @Override
     public void onDetach() {
         super.onDetach();
-
     }
 
-    /*
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-   /* public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }*/
 }
