7cd5432fcb2e1c4c7776de1ccbe79858365ea78d
Steen <steen.nisker@gmail.com>
2019-12-06 08:50:15 +0100

    Minor changes, nothing important

--- app/src/main/java/da/au_grp21/bluetoothdevelopmentdebugtool/ViewModel/MyViewModel.java
+++ app/src/main/java/da/au_grp21/bluetoothdevelopmentdebugtool/ViewModel/MyViewModel.java
@@ -28,9 +28,7 @@ import da.au_grp21.bluetoothdevelopmentdebugtool.R;
 public class MyViewModel extends ViewModel {
 
     private MutableLiveData<Device> devices;
-    //TODO:
     private ArrayList<Device> items;
-    //TODO:
     private MutableLiveData<List<Device>> numItems;
     private Device currentDevice;
 
