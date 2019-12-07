ddac4744d3bedc295bfdba3a131e0ac2c978b273
Tonni <tonni@witzell.dk>
2019-12-07 10:45:48 +0100

    Did some cleanup.

--- app/src/main/java/da/au_grp21/bluetoothdevelopmentdebugtool/Bluetooth/BluetoothConnectionService.java
+++ app/src/main/java/da/au_grp21/bluetoothdevelopmentdebugtool/Bluetooth/BluetoothConnectionService.java
@@ -153,110 +153,15 @@ public class BluetoothConnectionService extends IntentService{
                 }
             }
 
+            @Override
+                //  For broadcasting that new data is ready.
+            public void onCharacteristicChanged(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic) {
+                broadcastUpdate(ACTION_DATA_AVAILABLE, characteristic);
+            }
         };
 
 
 
-
-
-//        @Override
-//        public void onServicesDiscovered(BluetoothGatt gatt, int status){
-//            super.onServicesDiscovered(gatt, status);
-//
-//            List<BluetoothGattService> services = gatt.getServices();
-//            ArrayList<BluetoothGattCharacteristic> characteristics;
-//            List<BluetoothGattCharacteristic> gattCharacteristics;
-//            String uuid;
-//
-//            for (BluetoothGattService gattService : services){
-//                gattCharacteristics = gattService.getCharacteristics();
-//                characteristics = new ArrayList<>();
-//
-//                for (BluetoothGattCharacteristic gattCharacteristic : gattCharacteristics ){
-//                    characteristics.add(gattCharacteristic);
-//                    uuid = gattCharacteristic.getUuid().toString();
-//                    //  BLE data Notify
-//
-//                    if (uuid.equals("0000fff4-0000-1000-8000-00805f9b34fb")){
-//                        final int characteristicsProps = gattCharacteristic.getProperties();
-//                        if ((characteristicsProps | BluetoothGattCharacteristic.PROPERTY_NOTIFY) > 0){
-//                            BluetoothGattCharacteristic NotifyCharacteristic = gattCharacteristic;
-//                            setCharacteristicNotification(gattCharacteristic, true);
-//                        }
-//                    }
-//                }
-//            }
-//        }
-
-//        @Override
-//        public void onCharacteristicRead(BluetoothGatt gatt, BluetoothGattCharacteristic characteristic, int status){
-//            // Still connected to the Service
-//            if (status == BluetoothGatt.GATT_SUCCESS){
-//                // Send Characteristic via broardcast update
-//                BroadcastUpdate(ACTION_DATA_AVAILABLE, characteristic);
-//            }
-//        }
-
-
-
-
-//        public void setCharacteristicNotification(final BluetoothGattCharacteristic characteristic,
-//                                                  boolean enabled) {
-//            if (bluetoothAdapter == null || bluetoothGatt == null) {
-//                return;
-//            }
-//            bluetoothGatt.setCharacteristicNotification(characteristic, enabled);
-//
-//            Log.e("uuid service", characteristic.getUuid() + "");
-//            String uuid = "0000fff2-0000-1000-8000-00805f9b34fb";
-//
-//            if (uuid.equals(characteristic.getUuid().toString())) {
-//                Log.e("uuid service2", characteristic.getUuid() + "");
-//                BluetoothGattDescriptor descriptor = characteristic.getDescriptor(
-//                        UUID.fromString(SampleGattAttributes.CLIENT_CHARACTERISTIC_CONFIG));
-//                if (descriptor != null) {
-//                    descriptor.setValue(enabled ? BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE : new byte[]{0x00, 0x00});
-//                    bluetoothGatt.writeDescriptor(descriptor);
-//                }
-//            }
-//        }
-//
-//
-//        public void setCharacteristicNotification(final BluetoothGattCharacteristic characteristic, Boolean isEnabled){
-//            if (bluetoothAdapter == null || bluetoothGatt == null){
-//                return;
-//            }
-//            bluetoothGatt.setCharacteristicNotification(characteristic, isEnabled);
-//
-//            Log.e("UUID Service 1", characteristic.getUuid().toString() + "");
-//                //  BLE Read Time
-//            String uuid = "0000fff2-0000-1000-8000-00805f9b34fb";
-//
-//            if (uuid.equals(characteristic.getUuid().toString())){
-//                Log.e("UUID Service 2:", characteristic.getUuid().toString()+ "");
-////                BluetoothGattDescriptor descriptor = characteristic.getDescriptor(UUID.fromString(SampleGattAttributes.CLIENT_CHARACTERISTIC_CONFIG));
-////
-////
-////                if (descriptor != null) {
-////                    descriptor.setValue(isEnabled ? BluetoothGattDescriptor.ENABLE_NOTIFICATION_VALUE : new byte[]{0x00, 0x00});
-////                    bluetoothGatt.writeDescriptor(descriptor);
-////                }
-//            }
-//        }
-//    };
-
-//    // Get the serial data out of the characteristic.
-//    private void BroadcastUpdate(final String action, final BluetoothGattCharacteristic characteristic){
-//        // Looking for the correct characteristic
-//        final Intent intent = new Intent(action);
-//        if (TX_CHAR_UUID.equals(characteristic.getUuid())){
-//            //  Extract serial data from stream - and transmit via local broadcast manager
-//            intent.putExtra(EXTRA_DATA, characteristic.getValue());
-//            LocalBroadcastManager.getInstance(this).sendBroadcast(intent);
-//        }
-//
-//
-//    }
 
         //  Broadcaster for intent actions like connected / disconnected etc.
     private void broadcastUpdate(final String action) {
@@ -281,6 +186,7 @@ public class BluetoothConnectionService extends IntentService{
     }
 
 
+    private final IBinder binder = new LocalBinder();
 
     public IBinder onBind(Intent intent){
         return binder;
@@ -293,16 +199,10 @@ public class BluetoothConnectionService extends IntentService{
         return super.onUnbind(intent);
     }
 
-    private final IBinder binder = new LocalBinder();
-
         //  the binder for the bluetooth service
     public class LocalBinder extends Binder{
         BluetoothConnectionService getService() {return BluetoothConnectionService.this;}
     }
-
-//    public class LocalBinder extends Binder {
-//        BluetoothConnectionService getService() {return BluetoothConnectionService.this;}
-//    }
 
 
 
